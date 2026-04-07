7b66853e8b0861485cf2c6c82819b70c0f67e1c8
kanak12219836 <119718338+kanak12219836@users.noreply.github.com>
2026-04-07 20:06:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// var a=10;
+// var b=20;
+
+// console.log(a+b);
+
+
+// function fruit(item){
+//     console.log("fruit name is:" + item);
+// }
+// fruit("apple");
+         //import-> require
+var fs = require("fs");
+fs.writeFileSync("kanaksharma.txt","my name is kanak")
